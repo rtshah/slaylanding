d18a654a8e0eb2c181d174497dd678c6db08ab37
rtshah <rahultshah@gmail.com>
2024-12-13 19:49:24 -0600

    sheeeit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // The following are optional depending on your deployment needs
-  // basePath: '/your-repo-name', // Add this if your site isn't being served from the root domain
-  // assetPrefix: '/your-repo-name', // Add this if your assets aren't being served correctly
+  basePath: '/slaylanding',
+  assetPrefix: '/slaylanding',
 };
 
 export default nextConfig;
